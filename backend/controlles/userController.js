badef35a633b8847b2b99d4d8f27097667a50764
Vlad Donchak <vlad.donchak1996@gmail.com>
2023-10-14 18:28:45 +0300

    UI done. Connect to DB and start REST API build-up

--- backend/controlles/userController.js
+++ backend/controlles/userController.js
@@ -1,4 +1,6 @@
 import User from '../models/userModel.js'
+import bcrypt from 'bcryptjs'
+import generateTokenAndSetCookie from '../utils/helpers/genTokenAndSetCookie.js'
 
 const signupUser = async (req, res) => {
   try {
@@ -22,6 +24,8 @@ const signupUser = async (req, res) => {
     await newUser.save()
 
     if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res)
+
       res.status(201).json({
         _id: newUser._id,
         name: newUser.name,
@@ -41,4 +45,106 @@ const signupUser = async (req, res) => {
   }
 }
 
-export { signupUser }
+const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = await User.findOne({ username })
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || '')
+
+    if (!user || !isPasswordCorrect) return res.status(400).json({ message: 'Invalid username or password' })
+
+    generateTokenAndSetCookie(user._id, res)
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      username: user.username,
+    })
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    })
+    console.log('Error in loginUser: ', err.message)
+  }
+}
+
+const logoutUser = (req, res) => {
+  try {
+    res.cookie('jwt', '', { maxAge: 1 })
+    res.status(200).json({ message: 'User logged out successfully' })
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    })
+    console.log('Error in logoutUser: ', err.message)
+  }
+}
+
+const followUnFollowUser = async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const userToModify = await User.findById(id)
+    const currentUser = await User.findById(req.user._id)
+
+    if (id === req.user._id.toString()) return res.status(400).json({ message: 'You cannot follow/unfollow yourself' })
+
+    if (!userToModify || !currentUser) return res.status(400).json({ message: 'User not found' })
+
+    const isFollowing = currentUser.following.includes(id)
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } })
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } })
+      res.status(200).json({ message: 'User unfollowed successfully' })
+    } else {
+      // Follow user
+
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } })
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } })
+      res.status(200).json({ message: 'User followed successfully' })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    })
+    console.log('Error in followUnFollowUser: ', err.message)
+  }
+}
+
+const updateUser = async (req, res) => {
+  const { name, email, username, password, profilePicture, bio } = req.body
+  const userId = req.user._id
+
+  try {
+    let user = await User.findById(userId)
+    if (!user) return res.status(400).json({ message: 'User not found' })
+
+    if (req.params.id !== userId.toString()) return res.status(400).json({ message: "You cannot update other user's profile" })
+
+    if (password) {
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password, salt)
+      user.password = hashedPassword
+    }
+
+    user.name = name || user.name
+    user.email = email || user.email
+    user.username = username || user.username
+    user.profilePicture = profilePicture || user.profilePicture
+    user.bio = bio || user.bio
+
+    user = await user.save()
+
+    res.status(200).json({ message: 'Profile updated successfully', user })
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    })
+    console.log('Error in updateUser: ', err.message)
+  }
+}
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser }
